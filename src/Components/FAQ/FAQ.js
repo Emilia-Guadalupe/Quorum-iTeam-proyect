9824f5f4b9c1357e2423837ff102df31dce573ca
Emilia <m.emilia.guadalupe@gmail.com>
2021-04-24 19:52:05 -0300

    New Component

---
+++ src/Components/FAQ/FAQ.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './faqStyles.css';
+
+function FAQ(){
+    return(
+        <div id="questions-container">
+            <div>
+                <p>Hi!</p>
+                <p>Hi!</p>
+                <p>Hi!</p>
+            </div>
+            <div>
+                <p>Hi!</p>
+                <p>Hi!</p>
+                <p>Hi!</p>
+            </div>
+        </div>
+    )
+}
+
+export default FAQ;
