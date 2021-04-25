7c22c2125c0e6c204f8023874414d536e40de046
Emilia <m.emilia.guadalupe@gmail.com>
2021-04-25 14:19:59 -0300

    Updated the component

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './faqStyles.css';
 
+import Questions from './Questions';
+
 function FAQ(){
+
     return(
         <div id="questions-container">
-            <div>
-                <p>Hi!</p>
-                <p>Hi!</p>
-                <p>Hi!</p>
-            </div>
-            <div>
-                <p>Hi!</p>
-                <p>Hi!</p>
-                <p>Hi!</p>
+            <div id="title-container"><h3>FAQ</h3></div>
+            <div id="artfol-questions">
+                <div className="first-set">
+                    <Questions question={"Where can I get Artfol?"} />
+                    <Questions question={"Will Artfol be available on the PC?"} />
+                    <Questions question={"Will I be able to post not safe for work art?"} />
+                </div>
+                <div className="second-set">
+                    <Questions question={"Can I sign up if I am not an artist?"} />
+                    <Questions question={"Can I use the app if I don't do digital art?"} />
+                </div>
             </div>
         </div>
     )
