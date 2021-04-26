cab544cdce9b064adfb5bef4fd94824af91631e5
Emilia <m.emilia.guadalupe@gmail.com>
2021-04-26 19:17:58 -0300

    Added some conditional elements

--- src/Components/FAQ/Questions.js
+++ src/Components/FAQ/Questions.js
@@ -4,18 +4,19 @@ import './questionsStyles.css';
 //Images 
 import Polygon from '../../Images/Polygon_1.png';
 
-function Questions({question}){
+function Questions({question, answer}){
 
     const [showing, setShowing] = useState(false);
+    
 
     return(
         <div className="all-the-questions-container">
-        <div className="question-styles">
-            <img onClick={() => setShowing(!showing)} className="polygon-figure" src={Polygon} alt="polygon figure" />
+        <div onClick={() => setShowing(!showing)} className="question-styles">
+            <img id="polygon-figure" className={(showing === true ? 'rotation' : '')} src={Polygon} alt="polygon figure" />
             <p id="question-section" className="question-text">{question}</p>
         </div>
         {showing ? 
-        <div><p className="question-text">Lorem ipsum dolor sit amet.</p></div>
+        <div><p id="answer" className="question-text">{answer}</p></div>
         : null}
         </div>
     )
